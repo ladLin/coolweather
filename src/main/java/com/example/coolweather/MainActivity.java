54a2440adea71309fbe3c3072e13c65d4666811e
arno <1053690126@qq.com>
2020-01-23 19:58:05 +0800

    完成遍历省市县三级列表的功能。

--- src/main/java/com/example/coolweather/MainActivity.java
+++ src/main/java/com/example/coolweather/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import org.litepal.LitePal;
+import org.litepal.crud.LitePalSupport;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LitePal.initialize(this);
     }
 }
